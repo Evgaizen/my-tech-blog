da637da535678a4b956b86db66c28301d7a65713
Nikita Ivanov <49843824+Evgaizen@users.noreply.github.com>
2025-11-18 21:18:46 +0300

    Restyle layout

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -13,17 +13,19 @@ interface Props {
 
 export function Navbar({ links, avatar }: Props) {
   return (
-    <nav className="py-3 border-b-1 border-neutral-200 w-full flex justify-center shadow-sm">
-      {avatar}
-      {links.map((link) => (
-        <Link
-          className="mx-2 hover:bg-neutral-100 px-4 py-2 rounded-lg"
-          key={link.url}
-          href={link.url}
-        >
-          {link.text}
-        </Link>
-      ))}
+    <nav className="px-110 border-b-1 h-15 border-neutral-200 w-full flex justify-center shadow-sm items-center">
+      <div className="mx-2">{avatar}</div>
+      <div>
+        {links.map((link) => (
+          <Link
+            className="mx-2 my-auto hover:bg-neutral-100 px-4 py-2 rounded-lg"
+            key={link.url}
+            href={link.url}
+          >
+            {link.text}
+          </Link>
+        ))}
+      </div>
     </nav>
   );
 }
